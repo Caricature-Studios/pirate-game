c9cae6bbf61e9568b5410cc810041fe9a325de73
alex rusk <rusk.alex4@gmail.com>
2024-06-06 23:59:25 -0700

    can use modulate to tell if selected or not

--- Entities/MainBoat/MainBoat.cs
+++ Entities/MainBoat/MainBoat.cs
@@ -4,14 +4,21 @@ using System;
 public partial class MainBoat : CharacterBody2D
 {
 	private NavigationAgent2D _agent;
+    private Sprite2D _boatBody;
     private Attachment _portAttachment;
     private Attachment _starboardAttachment;
 
+    [Export]
+    private bool _isSelected;
+
 	private float _speed = 300.0f;
+
+
 
 	public override void _Ready()
     {
         _agent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+        _boatBody = GetNode<Sprite2D>("BoatBody");
         _portAttachment = GetNode<Attachment>("PortAttachment");
         _starboardAttachment = GetNode<Attachment>("StarboardAttachment");
 
@@ -19,15 +26,15 @@ public partial class MainBoat : CharacterBody2D
 
     public override void _Input(InputEvent @event)
     {
-        if (
-            @event is InputEventMouseButton e &&
-            !e.Pressed &&
-            e.ButtonIndex == MouseButton.Right
-        ) {
-            var map = GetWorld2D().NavigationMap;
-            var p = NavigationServer2D.MapGetClosestPoint(map, e.Position);
-            _agent.TargetPosition = p;
-        }
+        //if (
+        //    @event is InputEventMouseButton e &&
+        //    !e.Pressed &&
+        //    e.ButtonIndex == MouseButton.Right
+        //) {
+        //    var map = GetWorld2D().NavigationMap;
+        //    var p = NavigationServer2D.MapGetClosestPoint(map, e.Position);
+        //    _agent.TargetPosition = p;
+        //}
     }
     public override void _PhysicsProcess(double delta)
     {
@@ -39,5 +46,14 @@ public partial class MainBoat : CharacterBody2D
         Vector2 dir = diff.Normalized();
         Velocity = dir * _speed;
         MoveAndSlide();
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        if (_isSelected)
+            _boatBody.Modulate = Color.Color8(0,0,255);
+        else
+         _boatBody.Modulate = Color.Color8(255,255,255);
     }
 }
